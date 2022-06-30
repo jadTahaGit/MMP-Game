b101e639b796d472179555765db0e610943b61a8
Jad Taha <jadtaha.de@gmail.com>
2022-06-30 23:12:06 +0200

    AstroidController Script --version01.00.09

---
+++ Assets/astroidConroller.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class astroidConroller : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 2f;
+    private Rigidbody2D rigidbody2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D.velocity = new Vector2(0, -speed);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
