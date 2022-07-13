b084c21abc680859034e14ca37ec826a8c623492
Jad Taha <jadtaha.de@gmail.com>
2022-07-13 14:12:19 +0200

    garbageCollector --version01.00.19

--- Assets/Scripts/astroidConroller.cs
+++ Assets/Scripts/astroidConroller.cs
@@ -7,6 +7,7 @@ public class astroidConroller : MonoBehaviour
     [SerializeField]
     private float speed = 2f;
     private Rigidbody2D rigidbody2D;
+    private Vector2 screenBounds;
 
     // Start is called before the first frame update
     void Start()
@@ -14,11 +15,17 @@ public class astroidConroller : MonoBehaviour
 
         rigidbody2D = GetComponent<Rigidbody2D>();
         rigidbody2D.velocity = new Vector2(0, -speed);
+
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        // *2 ist ein pufer
+        if(-transform.position.y > screenBounds.y * 2){
+            Destroy(this.gameObject);
+        }
     }
 }
