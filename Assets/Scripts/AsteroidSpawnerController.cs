e1269ac488de7c33afac07d661166423ed81b300
Jad Taha <jadtaha.de@gmail.com>
2022-07-13 14:03:44 +0200

    onePerSecond --version01.00.18

--- Assets/Scripts/AsteroidSpawnerController.cs
+++ Assets/Scripts/AsteroidSpawnerController.cs
@@ -7,12 +7,14 @@ public class AsteroidSpawnerController : MonoBehaviour
 
     [SerializeField]
     private GameObject asteroidPrefab;
+    [SerializeField]
+    private float respawnTime = 1.0f;
     private Vector2 screenBounds;
     // Start is called before the first frame update
     void Start()
     {
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
-        SpawnAsteroid();
+        StartCoroutine(AsteroidWave());
     }
 
     // Update is called once per frame
@@ -21,5 +23,13 @@ public class AsteroidSpawnerController : MonoBehaviour
         GameObject asteroidInstance = Instantiate(asteroidPrefab) as GameObject;
         float spawnX = Random.Range(-screenBounds.x, screenBounds.x);
         asteroidInstance.transform.position = new Vector2(spawnX, 10);
+    }
+
+    private IEnumerator AsteroidWave(){
+        while(true){
+            yield return new WaitForSeconds(respawnTime);
+            SpawnAsteroid();
+
+        }
     }
 }
